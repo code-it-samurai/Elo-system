380a4663875220697945b73846603e86acd6dd06
code-it-samurai <prathameshtakane76@gmail.com>
2025-05-31 23:55:12 +0530

    Chore: cleaned up the debug print statements & initialized the calculation for new ratings function

--- elo_sys.py
+++ elo_sys.py
@@ -14,24 +14,20 @@ class EloSys:
         self.scaling_factor = 400
 
     def calculate_expected(self):
-        print("player1 rating", self.player1_rating)
-        print("player1 rating", self.player1_rating)
-
         rating_difference_player1 = self.player2_rating - self.player1_rating
         self.player1_win_expectancy = 1 / (
             1 + 10 ** ((rating_difference_player1) / 400)
         )
         self.player1_win_expectancy = (self.player1_win_expectancy / 1) * 100
-        print("Player 1's expected rating against Player 2")
-        print(round(self.player1_win_expectancy, 2))
 
         rating_difference_player2 = self.player1_rating - self.player2_rating
         self.player2_win_expectancy = 1 / (
             1 + 10 ** ((rating_difference_player2) / 400)
         )
         self.player2_win_expectancy = (self.player2_win_expectancy / 1) * 100
-        print("player 2's expected rating ")
-        print(round(self.player2_win_expectancy, 2))
+
+    def calculate_updated_ratings(self):
+        console.log("calculating change in ratings for both players")
 
 
 elo = EloSys(1600, 1400)
