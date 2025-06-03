0016430e9e6452b8d4de6bb016671ee133cbb689
code-it-samurai <prathameshtakane76@gmail.com>
2025-06-04 00:25:29 +0530

    Feature: added new rating calculation function

--- elo_sys.py
+++ elo_sys.py
@@ -1,22 +1,22 @@
 # Elo Ranking system
 # Currently supporting 2 player matches
 class EloSys:
-    def __init__(self, player1_rating, player2_rating):
+    def __init__(self, player1_rating, player2_rating, player1_score, player2_score):
         # current rating of each player
         self.player1_rating = player1_rating
         self.player2_rating = player2_rating
+
+        # who won 1 = victory, 0 = defeat, 0.5 = draw
+        self.player1_score = player1_score
+        self.player2_score = player2_score
 
         # expected rating of each player depending on their current rating and scaling factor
         self.player1_win_expectancy = 0
         self.player2_win_expectancy = 0
 
-        # who won 1 = victory, 0 = defeat, 0.5 = draw
-        self.player1_score = 0
-        self.player2_score = 1
-
         # used to calibrate the sensitivity of the rating system, higher the number -> drastic the changes to elo post match
         self.scaling_factor = 400
-        self.adjustment_factor = 32  # recommended default value
+        self.k_factor = 32  # recommended default value
 
     def calculate_expected(self):
         rating_difference_player1 = self.player2_rating - self.player1_rating
@@ -32,8 +32,9 @@ class EloSys:
         print(self.player1_win_expectancy, self.player1_rating)
         print(self.player2_win_expectancy, self.player2_rating)
 
-    def calculate_updated_ratings(self):
-        print("printing updated rating")
+    def new_ratings_calculator(self, score, win_expectancy):
+        new_rating = self.player1_rating + self.k_factor * (score - win_expectancy)
+        return new_rating
 
     def win_expectancy_calculator(self, rating_difference):
         win_expectancy_raw = 1 / (1 + 10 ** ((rating_difference) / self.scaling_factor))
